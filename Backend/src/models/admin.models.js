bfeb7e7819aff1b3fa9e5f0f9474515685d57186
Mrityunjay Tripathi <mrityunjaytripathi596@gmail.com>
2026-02-24 23:57:42 +0530

    Added password hashing and JWT auth methods to student and admin models and fixed some bugs in data models.

--- Backend/src/models/admin.models.js
+++ Backend/src/models/admin.models.js
@@ -1,6 +1,8 @@
-import mongoose from "mongoose";
+import mongoose,{Schema} from "mongoose";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
-const userSchema = new Schema({
+const adminSchema = new Schema({
     adminId: {
         type: String,
         required: [true, "userId is mandatory"], 
@@ -15,4 +17,44 @@ const userSchema = new Schema({
     }
 }, {timestamps: true});
 
-export const User = mongoose.model("User", userSchema);
+adminSchema.pre("save", async function()
+{
+    if(this.isModified("password"))
+    {
+        this.password = await bcrypt.hash(this.password, 10);
+    }
+})
+
+adminSchema.methods.checkPassword = async function(password)
+{
+    return await bcrypt.compare(password, this.password);
+}
+
+adminSchema.methods.generateAccessToken = function()
+{
+    return jwt.sign(
+        {
+            _id: this._id,
+            adminId: this.adminId
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
+}
+
+adminSchema.methods.generateRefreshToken = function()
+{
+    return jwt.sign(
+        {
+            _id: this._id,
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
+}
+
+export const Admin = mongoose.model("Admin", adminSchema);
--- Backend/src/models/students.models.js
+++ Backend/src/models/students.models.js
@@ -1,7 +1,9 @@
-import mongoose from "mongoose";
+import mongoose, {Schema} from "mongoose";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 const studentSchema = new Schema({    
-    id: {
+    studentId: {
         type: String,
         required: true
     },
@@ -58,5 +60,45 @@ const studentSchema = new Schema({
         type: String
     }
 }, {timestamps: true});
+
+studentSchema.pre("save", async function()
+{
+    if(this.isModified("password"))
+    {
+        this.password = await bcrypt.hash(this.password, 10);
+    }
+})
+
+studentSchema.methods.generateAccessToken = function()
+{
+    return jwt.sign(
+        {
+            _id: this._id,
+            studentId: this.studentId
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
+}
+
+studentSchema.methods.generateRefreshToken = function()
+{
+    return jwt.sign(
+        {
+            _id: this._id,
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
+}
+
+studentSchema.methods.checkPassword = async function (password)
+{
+    return await bcrypt.compare(password, this.password);
+}
 
 export const Student = mongoose.model("Student", studentSchema);
