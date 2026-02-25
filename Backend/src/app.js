9c7fe031e7429925338f66900dc2f3354e0751c8
Mrityunjay Tripathi <mrityunjaytripathi596@gmail.com>
2026-02-25 14:21:43 +0530

    Added routing and controller files for students and admins

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -8,4 +8,11 @@ app.use(express.urlencoded({extended: true, limit: "16kb"}));
 app.use(express.static("public"));
 app.use(cookieParser());
 
+//importing the routers
+import { studentRouter } from "./routes/students.routes.js";
+import { adminRouter } from "./routes/admin.routes.js";
+
+app.use("/api/v1/students", studentRouter);
+app.use("/api/v1/admins", adminRouter);
+
 export {app};
